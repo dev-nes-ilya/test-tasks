90eb39079490a76dab7b2d9f02140af2119e1804
Несмиянов Илья <IGenNesmiyanov@sberbank.ru>
2023-08-18 16:56:26 +0300

    first commit

---
+++ src/components/Schedule/Index.tsx
@@ -0,0 +1,40 @@
+import React, { useState } from "react";
+import { getSchedule } from "../../api/api";
+
+import styles from "./Schedule.css";
+
+// История
+// Хитрый бекендер поменял формат возвращаемых данных из API getSchedule
+// И все сломалось :)
+// Вчера он попытался починить компонент Schedule, но стало только хуже
+
+// Задача
+// Починить компонент Schedule :) и вывести список дней возвращаемых из getSchedule
+// В списке должны остаться только уникальные дни с понедельника по пятницу
+// В писке должны быть записи состоящие только из первого слова
+// "Понедельник"
+// "Вторник"
+
+// Условие
+// getSchedule возвращает Promise
+// Структуру самой функции getSchedule менять нельзя
+
+export default function Schedule() {
+  const [list, setList] = useState<any>([]);
+
+  getSchedule((data: any) => {
+    setList(data);
+  });
+
+  return (
+    <div>
+      <div className={styles.Header}>Дни недели</div>
+
+      <ul>
+        {list.map(({ item }: any) => (
+          <li>{item}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}

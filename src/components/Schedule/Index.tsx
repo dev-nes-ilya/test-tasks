f9f8a84ce7c7c6e388372383bd8f95882a5a1f14
Ilya Nesmiyanov <48203393+dev-nes-ilya@users.noreply.github.com>
2024-03-27 09:55:22 +0300

    Update Index.tsx

--- src/components/Schedule/Index.tsx
+++ src/components/Schedule/Index.tsx
@@ -10,29 +10,33 @@ import styles from "./Schedule.css";
 
 // Задача
 // Починить компонент Schedule :) и вывести список дней возвращаемых из getSchedule
-// В списке должны остаться только уникальные дни с понедельника по пятницу
+// В списке должны остаться только уникальные дни с понедельника по пятницу. 
 // В списке должны быть записи, состоящие только из первого слова
 // "Понедельник"
 // "Вторник"
+// Массив с данными нужно преобразовать таким образом, чтобы callback для list.map() отобразил список. 
 
 // Условие
 // getSchedule возвращает Promise
-// Структуру самой функции getSchedule менять нельзя
+// Структуру объявления функции getSchedule менять нельзя
+// Строки кода, находящиеся после return менять нельзя. 
 
 export default function Schedule() {
   const [list, setList] = useState<any>([]);
+  const excludedDays = ["Суббота","Воскресение"]
 
   getSchedule((data: any) => {
     setList(data);
   });
 
+// код ниже не должен меняться
   return (
     <div>
       <div className={styles.Header}>Дни недели</div>
 
       <ul>
-        {list.map(({ item }: any) => (
-          <li>{item}</li>
+        {list.map(({ item, id }: any) => (
+          <li key={id}>{item}</li>
         ))}
       </ul>
     </div>
